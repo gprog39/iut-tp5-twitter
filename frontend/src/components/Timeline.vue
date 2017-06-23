cc880a615f4c49a8ff2b53652015a64bb6b7bb6b
gprog39 <pyfes@aol.com>
2017-06-23 12:01:21 +0200

    Etape 8 réalisée

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -1,61 +1,57 @@
 <template>
   <div class="timeline">
-    <h1>TWEETS</h1>
-    <ul>
-      <li v-for="tweet in tweets">
-        <tweet :tweet="tweet"/>
-      </li>
-    </ul>
+    <h1> Tweets </h1>
+    <feed :tweets="tweets" :loading="loading"/>
   </div>
 </template>
 
 <script>
-import Tweet from './Tweet'
-import Vue from 'vue'
-import Resource from 'vue-resource'
-Vue.use(Resource)
-export default{
-  name: 'timeline',
-  data () {
-    return {
-      tweets: [{auteur: 'pp', contenu: 'haha'}, {auteur: 'jj', contenu: 'bg'}, {auteur: 'ff', contenu: 'hihihiahhha'}]
+  import Feed from './Feed'
+  import Vue from 'vue'
+  import Resource from 'vue-resource'
+  Vue.use(Resource)
+  export default {
+    name: 'timeline',
+    data () {
+      return {
+        tweets: [],
+        loading: true
+      }
+    },
+    components: {Feed},
+    methods: {
+      fetchTweets: function () {
+        this.$http.get('http://localhost:8080/list').then(response => {
+          // get body data
+          this.tweets = response.body
+          this.loading = false
+          console.log(this.tweets)
+          console.log(this.loading)
+        }, response => {
+          // error callback
+        })
+      }
+    },
+    created () {
+      this.fetchTweets()
     }
-  },
-  components: {Tweet},
-  methods: {
-    fetchTweets: function () {
-      this.$http.get('http://localhost:8080/list').then(response => {
-       // get body data
-        this.tweets = response.body
-        console.log(response.body)
-      }, response => {
-         // error callback
-      })
-    }
-  },
-  created () {
-    this.fetchTweets()
   }
-}
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
